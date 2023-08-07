fda7ec72ccf24948d4b158dcd197a6484d810c04
harmony <harmony.dgt@gmail.com>
2023-08-07 18:05:17 -0300

    ..

---
+++ assets/js/app.js
@@ -0,0 +1,35 @@
+const app = document.getElementById('app')
+
+console.log(app)
+
+
+if(app){
+    app.innerHTML=`
+       
+        <main>  
+        
+            <div class="content-text">
+                <h1>
+                    grupo VIP de r$97 por zer0 até hoje as 23:59
+                </h1>
+            </div>
+
+            <div class='cover'>
+                 <img src='assets/img/cover-mb.png'>
+            </div>
+
+            <div>
+
+                <h2>
+                     Clique no botão abaixo e acesse agora!
+
+                </h2>
+            <a class="btn app" href="https://t.me/ComunidadeCassinoMilionario">quero entrar na </br> comunidade vip agora</a>
+            </div>
+
+        </main>
+        <footer>
+             Copyright 2023® - Manauara  Termos de Uso | Políticas de Privacidade
+        </footer>
+    `
+}
